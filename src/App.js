a105dd2ccb23720583eab6c71f6a201d43c36533
@sfikizol021 <sfikizol021@student.wethinkcode.co.za>
2023-03-17 13:44:19 +0200

    able to get data from api and display it on console

--- src/App.js
+++ src/App.js
@@ -3,10 +3,59 @@ import axios from "axios";
 
 function App() {
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=CapeTown&appid=45c11a32310922009298132595529fed"
+  const [data,setData] = useState({})
+  const [location,setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=45c11a32310922009298132595529fed`
+
+  const getLocation = (event) =>{
+    if(event.key =='Enter'){
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+
+    }
+   
+  }
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div className="app">
+      <div className="EnterLocation">
+      <input value={location}
+             onChange={event => setLocation(event.target.value)}
+             onKeyDown = {getLocation}
+             placeholder = "Enter a location"
+             type ="text"/>
+      </div>
+     <div className="container">
+      <div className="top">
+      <div className="location">
+        <p>Johannesburg</p>
+      </div>
+      <div className="temp">
+          <h1>18°C</h1>
+      </div>
+      <div className="des">
+          <h1>partly cloudy</h1>
+      </div>
+      </div>
+      <div className="bottom">
+      <div className="feels">
+          <h1>23</h1>
+          <h3>feels like</h3>
+      </div>
+      <div className="humidity">
+          <h1>10%</h1>
+          <h3>humidity</h3>
+      </div>
+      <div className="wind">
+          <h1>12 MPH</h1>
+          <h3>Wind Speed</h3>
+      </div>
+      </div>
+     </div>
+
     </div>
   );
 }
